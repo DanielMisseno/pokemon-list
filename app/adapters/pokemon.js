b5ac97f53206e0a219dcb2589181f58f57589163
Daniel Misseno <danielvieira@ext.cvc.com.br>
2018-01-15 10:47:07 -0200

    Fix envio de header pokemon-edit somente quando for edição

--- app/adapters/pokemon.js
+++ app/adapters/pokemon.js
@@ -4,7 +4,7 @@ import { get } from '@ember/object';
 export default BaseAdapter.extend({
 	updateRecord(store, type, snapshot) {
 		let data = snapshot.record.serialize();
-		let headers = Object.assign(get(this, 'headers'), { "pokemon-edit": true });
+		let headers = Object.assign({}, get(this, 'headers'), { "pokemon-edit": true });
 		let url = this.buildURL(type.modelName, snapshot.id, snapshot, 'updateRecord');
 		return this.ajax(url, 'PUT', { headers, data });
 	}
